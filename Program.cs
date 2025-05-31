e0bc9c994c72fa773c6eb37b0fc9da52d9699df3
Renan Coutinho <renan.coutinho.11@hotmail.com>
2025-05-31 10:53:06 -0300

    update1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("11999999999", "G20", "1234567890", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("11999999999", "13 Pro", "0987654321", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
